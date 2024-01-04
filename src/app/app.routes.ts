2bcb023a3ca57f4a34612de0130cbe7b2f59654e
지연우 <yanoo8184@gmail.com>
2024-01-04 18:13:05 +0900

    컴포넌트 및 라우터 설정

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: 'write',
+        loadComponent: () => import('./pages/write/write.component').then(m => m.WriteComponent)
+    },
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    }
+];
